a5a84b80a4457fb755f86edf3babf3cebb618200
Navraj Singh Kaler <31569537+navi1996@users.noreply.github.com>
2019-04-10 16:40:46 +0530

    Create restaurant.js

---
+++ FDWebService/src/model/restaurant.js
@@ -0,0 +1,9 @@
+class Restaurant {
+    constructor(obj) {
+        this.restaurantName = obj.restaurantName,
+        this.location = obj.location,
+        this.menu = obj.menu
+    }
+}
+
+module.exports = Restaurant;
